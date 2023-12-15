f415d3ee4ddb387c0c9c8b07a5658415b76c8e21
WiktorTobota <153942426+WiktorTobota@users.noreply.github.com>
2023-12-15 19:50:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from DownloadingData import Download_data
-
-
+from Create_csv import create_csv
+import datetime
 URL = 'https://www.amazon.co.uk/Amazon-Essentials-Relaxed-fit-Short-Sleeve-Crewneck/dp/B07XLLKRWH/ref=sr_1_1_ffob_sspa?crid=2EHOLBP25D34V&keywords=t-shirts&qid=1702564666&sprefix=t%2Bs%2Caps%2C120&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1&psc=1'
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
 
@@ -14,3 +14,10 @@ out = findall(pattern, price)
 price = out[0]
 price = price[1:] 
 title = title.strip()
+
+#getting todays date
+today = datetime.date.today()
+
+#creating new csv file -> do only once! 
+create_csv(title, price, today)
+

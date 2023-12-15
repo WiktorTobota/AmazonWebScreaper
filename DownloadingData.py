bf4a590262129fe7d0bce4e16ffc39f13374e05a
WiktorTobota <153942426+WiktorTobota@users.noreply.github.com>
2023-12-15 19:17:53 +0100

    Update DownloadingData.py

--- DownloadingData.py
+++ DownloadingData.py
@@ -1,1 +1,11 @@
+from bs4 import BeautifulSoup
+
+def Download_data(URL, headers):
+  page = requests.get(URL, headers=headers)
+  soup1 = BeautifulSoup(page.content, "html.parser")
+  soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+
+  title = soup2.find(id='productTitle').get_text()
+  price = soup2.find(id="corePrice_desktop").get_text()
+  return title, price
 

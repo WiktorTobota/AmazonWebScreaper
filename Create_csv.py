ca702b96d8f9d16ab6730e26809d33f2ec0ddf7a
WiktorTobota <153942426+WiktorTobota@users.noreply.github.com>
2023-12-16 17:53:29 +0100

    Update Create_csv.py

--- Create_csv.py
+++ Create_csv.py
@@ -1,9 +1,9 @@
 import csv 
 def create_csv(title, price, date):
-  header = ['Title ', ' Price', 'Date']
-  data = [title, price, date]
+    header = ['Title ', ' Price', 'Date']
+    data = [title, price, date]
 
-  with open('AmazonProducts.csv', 'w', newline='', encoding='UTF8') as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
-    writer.writerow(data)
+    with open('AmazonProducts.csv', 'w', newline='', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerow(data)
